ababe27e86f7d4b21bf000611008a42a5b54e8ac
monosolo159 <wichetpong159@hotmail.com>
2017-11-26 08:17:10 +0700

    0

--- src/pages/car-service/car-service.ts
+++ src/pages/car-service/car-service.ts
@@ -17,6 +17,7 @@ export class CarServicePage {
 
   @ViewChild('mapContainer') mapContainer: ElementRef;
   map: any;
+  infoWindows: any;
 
   public xxxxx = 'https://www.w3schools.com/css/trolltunga.jpg';
   public data_table: Array<{}>;
@@ -24,52 +25,32 @@ export class CarServicePage {
   public mapApi ='AIzaSyBeyl5wC-Q1wBUqQh38lMITihEIolEikAo';
   public linkPic = this.server.linkServerPic();
   constructor(public loadingCtrl: LoadingController, public navCtrl: NavController, public alertCtrl: AlertController, public modalCtrl: ModalController, public storage: Storage, public server: Server, public http: Http) {
-
+    this.infoWindows = [];
   }
 
-  ionViewWillEnter() {
+  // ionViewWillEnter() {
+  //   this.displayGoogleMap();
+  //   this.getMarkers();
+  // }
+
+  ionViewDidEnter() {
     this.displayGoogleMap();
     this.getMarkers();
   }
-
-
-
-  // displayGoogleMap() {
-  //   // var lat;
-  //   // var long;
-  //
-  //   // this.geolocation.getCurrentPosition().then((resp) => {
-  //   //  lat = resp.coords.latitude
-  //   //  long =  resp.coords.longitude
-  //   //  console.log(lat+","+long);
-  //   // }).catch((error) => {
-  //   //   console.log('Error getting location', error);
-  //   // });
-  //
-  //   let latLng = new google.maps.LatLng(17.150914, 104.153813);
-  //
-  //   let mapOptions = {
-  //     center: latLng,
-  //     disableDefaultUI: true,
-  //     zoom: 11,
-  //     mapTypeId: google.maps.MapTypeId.ROADMAP
-  //   }
-  //   this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
-  // }
 
   displayGoogleMap() {
     let latLng = new google.maps.LatLng(17.150914, 104.153813);
 
     let mapOptions = {
       center: latLng,
-      disableDefaultUI: true,
+      disableDefaultUI: false,
+      fullscreenControl: false,
       zoom: 15,
+      myLocationButton: true,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":20},{"color":"#ececec"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"on"},{"color":"#f0f0ef"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#f0f0ef"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"visibility":"on"},{"color":"#d4d4d4"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"on"},{"color":"#ececec"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21},{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#d4d4d4"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#303030"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"saturation":"-100"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"geometry.stroke","stylers":[{"lightness":"-61"},{"gamma":"0.00"},{"visibility":"off"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#dadada"},{"lightness":17}]}]
     }
     this.map = new google.maps.Map(this.mapContainer.nativeElement, mapOptions);
   }
-
 
   getMarkers() {
     this.http.get('assets/markers.json')
@@ -82,8 +63,32 @@ export class CarServicePage {
   addMarkersToMap(markers) {
     for(let marker of markers) {
       var position = new google.maps.LatLng(marker.latitude, marker.longitude);
-      var dogwalkMarker = new google.maps.Marker({position: position, title: marker.title});
+      var dogwalkMarker = new google.maps.Marker({
+        position: position,
+        title: marker.name,
+        description : marker.description
+        });
       dogwalkMarker.setMap(this.map);
+      this.addInfoWindowToMarker(dogwalkMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker) {
+    var infoWindowContent = '<div id="content"><h3 id="firstHeading" class="firstHeading">' + marker.title + '</h3></div>';
+    var infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+    marker.addListener('click', () => {
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for(let window of this.infoWindows) {
+      // window.open('geo://' + 17.150914 + ',' + 104.153813 + '?q=' + 17.15106291 + ',' + 104.15644169 + '(สกลแกรนพาเรส)', '_system');
+      window.close();
     }
   }
 
